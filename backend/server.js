43cf021c900868c0e322440fc622ba779b309f87
Sayyedarham <115995089+Sayyedarham@users.noreply.github.com>
2026-02-09 23:24:50 +0530

    Set up Express server with CORS and static files

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-
+const app = express();
+const path = require('path');
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/countries', require('./routes/countries'));
